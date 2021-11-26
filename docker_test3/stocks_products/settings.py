1e2afc05a8b756d76dc0161223bae28b8be232c5
Afanasev Mikhail <82053882+headsoft-mikhail@users.noreply.github.com>
2021-11-27 01:25:24 +0300

    DEBUG value converts to bool

--- docker_test3/stocks_products/settings.py
+++ docker_test3/stocks_products/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j"
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = bool(os.getenv('DJANGO_DEBUG'))
 
 ALLOWED_HOSTS = get_host(os.getenv('DJANGO_ALLOWED_HOSTS'))
 # Application definition
